715bfcc94a6f578cd75968d33f0f19ed78e3b931
Alvin Ashiatey <alvinashiatey@gmail.com>
2023-08-25 14:24:44 -0400

    artist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "4e5m3wpi",
+      dataset: "development",
+      apiVersion: "2021-03-25",
+      useCdn: true,
+    }),
+  ],
+});
---
+++ src/data/api.ts
@@ -0,0 +1,46 @@
+import { useSanityClient } from "@sanity/astro";
+import type { PortableTextBlock } from "@portabletext/types";
+import type { ImageAsset, Slug } from "@sanity/types";
+import groq from "groq";
+
+export async function getArtists(): Promise<Artist[]> {
+  const query = groq`*[_type == "artist"]`;
+  const artists = await useSanityClient().fetch(query);
+  return artists;
+}
+
+export async function getArtistBySlug(slug: string): Promise<Artist> {
+  return await useSanityClient().fetch(
+    groq`*[_type == "artist" && slug.current == $slug][0]`,
+    { slug }
+  );
+}
+
+export async function getSiteSettings(): Promise<Settings> {
+  const query = groq`*[_type == "settings" && _id == "settings"][0]`;
+  const siteSettings = await useSanityClient().fetch(query);
+  return siteSettings;
+}
+
+interface Link {
+  _type: string;
+  url: string;
+}
+
+interface Artist {
+  _type: string;
+  _id: string;
+  name: string;
+  slug: Slug;
+  image: ImageAsset;
+  imageCredit: string;
+  links: Link[];
+  bio: PortableTextBlock[];
+}
+
+interface Settings {
+  _type: string;
+  _id: string;
+  title: string;
+  description: string;
+}
