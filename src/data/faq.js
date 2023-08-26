b4b16870b9083f4159571130922fdc93c81d51d7
Mianwei-Wang <wmw601@outlook.com>
2023-08-26 17:18:32 -0400

    artist list pg

---
+++ src/data/faq.js
@@ -0,0 +1,27 @@
+const faqData = {
+    title: "Frequently Asked Questions",
+    data:[
+        {
+            question: "Do I have to be Asian American to exhibit or participate in Asian Arts Initiative?",
+            answer: "We define Asian America expansively, not exclusively. Anyone who identifies as a part of the Asian diaspora counts. We also invite anyone working in or within proximity of Asian American communities and our various issues. The Initiative was founded as an interracial coalition of Black and Asian American poets working together to respond to community issues through an arts lens, and we continue to believe in art strategies that center all of our voices."
+        }, {
+            question: "What kind of art do you exhibit?",
+            answer: "As a multi-disciplinary producer, presenter and anchor organization, our definition of art is open and questioning. We support artists in every facet of their work—whether it is to provide space to practice, perform or exhibit art, to sponsor the production of it, or just to let you know what we think of it. We strive to serve the Philadelphia arts community, and courageous work in emerging and emergent stages. We hold open calls for visual and performance art, community based projects, on a thematically seasonal basis."
+        }, {
+            question: "How do you select artists?",
+            answer: "We have a number of different methodologies for public programming, including guest curation, juried open calls, organizational partnerships. We have an annually renewed Community Advisory Board that informs us of the needs and desires of the constituents that matter most: our neighbors."
+        }, {
+            question: "Who are your constituents?",
+            answer: "We serve anyone who believes in the power of art to bring positive material change. Our building is a brave space for people of all extractions and contexts--generational, demographic, emotional, physical. Learn how we honor each other at the Initiative, with our community agreements."
+        }, {
+            question: "Who are your supporters?",
+            answer: "Some of our greatest supporters are on the Board of Directors, who not only assume responsibility for but represent the spirit and joy of our work. Many of our major projects are also made possible by the generous support of public and private foundations. You will see them acknowledged accordingly, on Exhibition, Event and Project pages."
+        }, {
+            question: "How do I join in supporting AAI?",
+            answer: "You can donate to the Initiative today! If you're interested in supporting us with your time and expertise, please visit our About page for contact information. All of this information and more is available in the links below."
+        }
+    
+    ]
+}
+
+export default faqData
