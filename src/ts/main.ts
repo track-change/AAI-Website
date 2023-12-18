3a4917515b2f8763bf44e5f9666986ab871afbed
mengjie-liu <mengjie.liu@yale.edu>
2023-12-17 23:01:17 -0500

    transition wip

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,17 @@
 import Alpine from "alpinejs";
+import barba from "@barba/core";
+import barbaCss from "@barba/css";
 
 declare global {
   interface Window {
     Alpine: typeof Alpine;
+    barba: typeof barba;
+    barbaCss: typeof barbaCss;
   }
 }
 
 window.Alpine = Alpine;
+window.barba = barba;
+window.barbaCss = barbaCss;
+barba.use(barbaCss);
 Alpine.start();
